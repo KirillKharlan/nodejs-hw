746c7bf98283f98d6c3eb72a84c27552f363513d
KirillKharlan <151565579+KirillKharlan@users.noreply.github.com>
2025-09-19 19:46:21 +0300

    hw3or4

--- hw.js
+++ hw.js
@@ -1,4 +1,9 @@
+const express = require('express')
+const HOST = "127.0.0.1"
+const PORT = 8000
+const app = express()
 const moment = require('moment');
+
 
     function getDate() {
       const date = moment();
@@ -17,3 +22,12 @@ const moment = require('moment');
 
     const currentDay = getCurrentWeekDay();
     console.log(currentDay);
+
+app.get("/timestamp", (reg, res)=>{
+  res.json({currentTime: currentDate})
+})
+
+app.listen(PORT, HOST, ()=>{
+  console.log(`http://${HOST}:${PORT}`)
+})
+

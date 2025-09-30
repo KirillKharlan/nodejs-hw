f18e7ae9741939d514c410128eb747da8e300f76
KirillKharlan <151565579+KirillKharlan@users.noreply.github.com>
2025-09-30 20:35:07 +0300

    usershw

--- hw.js
+++ hw.js
@@ -6,8 +6,11 @@ const PORT = 8000
 const app = express()
 const moment = require('moment');
 const jsonPath = path.join(__dirname, "posts.json") 
+const jsonUsersPath = path.join(__dirname, "users.json")
+const users = JSON.parse(fs.readFileSync(jsonUsersPath, 'utf-8'))
 const posts = JSON.parse(fs.readFileSync(jsonPath, 'utf-8'))
   console.log(posts)
+  console.log(users)
 
     function getDate() {
       const date = moment();
@@ -32,6 +35,7 @@ app.get("/timestamp", (req, res)=>{
   res.json({currentTime: currentDate})
 })
 
+// по запиту /posts якщо не вказано skip та take отримаємо усі пости, якщо вказан skip то пропускаємо стільки постів скільки вказано, якщо вказан take то беремо стільки постів скільки вказано
 app.get('/posts', (req, res) => {
   const skipC = req.query.skip;
   const takeC = req.query.take;
@@ -39,6 +43,7 @@ app.get('/posts', (req, res) => {
   let skip = 0;
   let take;
 
+  //Перевіряємо чи правильно вказан skip та взагалі чи він вказан
   if (skipC !== undefined && skipC !=='') {
     skip = parseInt(skipC);
     if (isNaN(skip) || skip < 0) {
@@ -46,6 +51,7 @@ app.get('/posts', (req, res) => {
     }
   }
 
+  //Перевіряємо чи правильно вказан take та взагалі чи він вказан
   if (takeC !== undefined && takeC !=='') {
     take = parseInt(takeC);
     if (isNaN(take) || take < 0) {
@@ -53,8 +59,10 @@ app.get('/posts', (req, res) => {
     }
   }
 
+  //Пропускаємо стільки постів скільки вказано в skip
   let resultPosts = posts.slice(skip);
 
+  //Беремо стільки постів скільки вказано у take
   if (take !== undefined) {
     resultPosts = resultPosts.slice(0, take);
   }
@@ -64,7 +72,7 @@ app.get('/posts', (req, res) => {
   res.status(200).json()
 });
 
-
+// шукаємо пости по id, беремо id який користувач указав в запиті та переводино його в числовий тип данних, потім шукаємо його по цьому id в массиві, якщо такий пост є то він виводиться як відповідь користувачу, якщо його немає то ми відправляємо помилку 404 користувачу 
 app.get('/posts/:id', (req, res) => {
   const postId = parseInt(req.params.id);
 
@@ -81,10 +89,56 @@ app.get('/posts/:id', (req, res) => {
     res.status(200).json()
 });
 
-  
+
+app.get('/users/:id', (req, res) => {
+  // Пошук користувача, usersId робимо з id число, userFound шукаємо користувача в масиві
+  const usersId = parseInt(req.params.id);
+  const userFound = users.find(user => user.id === usersId);
+  // Перевіряємо чи є такий користувач, якщо ні то повертаємо помилку що такого користувача нема
+  if (!userFound) {
+    return res.status(404).send({error: `Користувача з ID ${usersId} не знайдено.`});
+  }
+  // Вилучаємо id з фінального массиву який буде відображено на сторінці
+  const { id, ...fList } = userFound; 
+  // Ініціалізуємо массив для полів які були вказані у запиті
+  let fieldsToSelect = [];
+  // Обробка query параметру fields де ми перевіряємо запит на усі помилки; .split(',') розбиваємо строку на , ; .map(field => field.trim()) видаляємо пробіли поблизу ім'я поля ; фільтруємо порожні строки.
+  if (req.query.fields) {
+    fieldsToSelect = req.query.fields
+      .split(',')
+      .map(field => field.trim()) 
+      .filter(field => field.length > 0);
+  }
+  // фінальний об'єкт який відправляється користувачу
+  const responseObject = {};
+  // Повертаємо увесь об'єкт користувача id якого вказано
+  if (fieldsToSelect.length === 0) {
+    // Копіюємо усі властивості з flist без id у responseObject 
+    Object.assign(responseObject, fList);
+  } 
+  // ФІльтруємо запит та виводимо тільки ті поля які вказані
+  else {
+    // шукаємо чи є поля які вказали в запиті в массиві, і якщо є то додаємо їх у відповідь
+    for (const field of fieldsToSelect) {
+      if (fList.hasOwnProperty(field)) {
+        responseObject[field] = fList[field];
+      } 
+    }
+  }
+  // відправка відповіді
+  res.json(responseObject)
+
+    res.status(200).json()
+});
+
+// Отримаємо усіх користувачів по запиту /users
+app.get("/users", (reg, res)=>{
+  res.json({allUsers: users})
+})
 
 
 app.listen(PORT, HOST, ()=>{
   console.log(`http://${HOST}:${PORT}/posts?skip=2&take=2`)
+  console.log(`http://${HOST}:${PORT}/user/1?fields=name,email`)
 })
 
